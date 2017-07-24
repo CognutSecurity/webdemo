fdaff71c23fba6827530c2b1ae3a5c0227cebf7f
Xiao, Huang <xiaohu@in.tum.de>
2017-07-24 17:15:56 +0200

    - decompose templates in elements - easy extensible - home site ready - login/reset/register sites ready

--- views/apps/Home.py
+++ views/apps/Home.py
@@ -3,9 +3,22 @@ import jinja2 as jj2
 
 class Home(object):
    def __init__(self):
-      self.env = jj2.Environment(loader=jj2.FileSystemLoader('./templates/Home'))
+      self.env = jj2.Environment(loader=jj2.FileSystemLoader('./templates'))
 
    @cherrypy.expose
    def index(self):
       app_list = cherrypy.request.app.config['APPS']['app_list']
-      return self.env.get_template('index.html').render(apps=app_list)
+      return self.env.get_template('Home/index.html').render({"apps": app_list,
+                                                          "page_subtitle": "Home"})
+
+   @cherrypy.expose
+   def signup(self):
+      return self.env.get_template('signup.html').render(page_subtitle="Sign Up")
+
+   @cherrypy.expose
+   def login(self):
+      return self.env.get_template('login.html').render(page_subtitle="Login")
+
+   @cherrypy.expose
+   def reset(self):
+      return self.env.get_template('reset.html').render(page_subtitle="Reset Password")
